6f24e63e459efb9f455d55c725469684621e1559
BullyHunter32 <60613196+BullyHunter32@users.noreply.github.com>
2023-02-02 14:15:47 +0000

    pointer to GetLocalPlayer(). dll unloading.

--- dllmain.cpp
+++ dllmain.cpp
@@ -3,11 +3,13 @@
 #include "Interfaces.h"
 #include "globals.h"
 #include "hooks.h"
+#include "c_gmod_player.h"
 
 CInterfaces* Interface = nullptr;
 CDebugConsole* g_pDebug = nullptr;
 CGlobalVars* g_pGlobals = nullptr;
-BOOL WINAPI MainThread(LPVOID)
+HMODULE hDll;
+BOOL WINAPI MainThread(HMODULE hThread)
 {
 	g_pDebug = new CDebugConsole();
 	g_pDebug->CreateInstance();
@@ -17,6 +19,20 @@ BOOL WINAPI MainThread(LPVOID)
 	Interface = new CInterfaces();
 	Hooks = new CHooks();
 
+	DWORD callLocalPlayer = (DWORD)((DWORD)(*(void***)Interface->ClientModeShared)[21] + 4);
+	DWORD jmp = *(DWORD*)callLocalPlayer;
+	GetLocalPlayer = (C_GMOD_Player*(*)())(callLocalPlayer + jmp + 4);
+
+	g_pDebug->Print("EntIndex: %s\n", ((C_GMOD_Player*)(Interface->ClientEntityList->GetClientEntity(2)))->GetName());
+
+	player_info_t info;
+	Interface->EngineClient->GetPlayerInfo(2, &info);
+	g_pDebug->Print("name: %s\n", info.name);
+
+	// name offset 30a4 ?
+	
+
+	//g_pDebug->Print("isdormant: %d\n", GetLocalPlayer()->GetClientNetworkable()->EntIndex());
 	// taking this from CHLClient::HudUpdate as it's pretty much the first instruction, easy to get
 	g_pGlobals = (CGlobalVars*)(**(DWORD***)(void*)(
 		(uintptr_t)(*(void***)Interface->Client)[11] + 5
@@ -24,10 +40,7 @@ BOOL WINAPI MainThread(LPVOID)
 
 	g_pDebug->Print("Setting up hooks...\n");
 
-	Interface->Dump("client.dll");
-	Interface->Dump("engine.dll");
-
-	Hooks->CreateMove->Hook();
+	// Hooks->CreateMove->Hook();
 
 	g_pDebug->Print("So far so good\n");
 
@@ -36,15 +49,22 @@ BOOL WINAPI MainThread(LPVOID)
 	Hooks->Release();
 	Sleep(100);
 	delete Interface;
+	Sleep(50);
 	g_pDebug->Release();
+	Sleep(100);
+	FreeLibraryAndExitThread(hDll, 0);
+	Sleep(100);
+	FreeLibraryAndExitThread(hThread, 0);
 	return TRUE;
 }
 
-BOOL APIENTRY DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID)
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID)
 {
 	if (dwReason == DLL_PROCESS_ATTACH)
 	{
+		DisableThreadLibraryCalls(hModule);
 		CloseHandle(CreateThread(0, 0, (LPTHREAD_START_ROUTINE)MainThread, 0, 0, 0));
+		hDll = hModule;
 	}
 	return TRUE;
 }
