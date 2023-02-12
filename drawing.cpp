e4fb43c7770ffa35a132c3a0e1ccbf736e2794a1
RegiSimkus <60613196+BullyHunter32@users.noreply.github.com>
2023-02-12 13:12:42 +0000

    Added DrawText(Screen)A methods to drawing wrapper

--- drawing.cpp
+++ drawing.cpp
@@ -26,3 +26,29 @@ void CDrawingWrapper::DrawWorldLine(const Vector& start, const Vector& end, ImCo
 
 	DrawLine(screenStart, screenEnd, col, thickness);
 }
+
+void CDrawingWrapper::DrawTextA(const char* text, const ImVec2& pos, ImColor col, TextAlign alignx, TextAlign aligny)
+{
+	const char* textend = text + strlen(text);
+
+	ImVec2 size = ImGui::CalcTextSize(text, textend, false);
+	ImVec2 adjustedPos = pos;
+
+	if (alignx == TEXT_ALIGN_RIGHT)
+		adjustedPos.x -= size.x;
+	else if (alignx == TEXT_ALIGN_CENTER)
+		adjustedPos.x -= size.x / 2.f;
+	if (aligny == TEXT_ALIGN_BOTTOM)
+		adjustedPos.y -= size.y;
+	else if (aligny == TEXT_ALIGN_CENTER)
+		adjustedPos.y -= size.y / 2.f;
+
+	DrawList->AddText(adjustedPos, col, text, textend);
+}
+
+void CDrawingWrapper::DrawTextScreenA(const char* text, const Vector& pos, ImColor col, TextAlign alignx, TextAlign aligny)
+{
+	Vector screenpos;
+	Interface->DebugOverlay->ScreenPosition(pos, screenpos);
+	DrawTextA(text, screenpos, col, alignx, aligny);
+}
--- drawing.h
+++ drawing.h
@@ -2,6 +2,15 @@
 #include "ImGui/imgui_internal.h"
 #include "vector.h"
 
+enum TextAlign
+{
+	TEXT_ALIGN_LEFT,
+	TEXT_ALIGN_TOP,
+	TEXT_ALIGN_RIGHT,
+	TEXT_ALIGN_BOTTOM,
+	TEXT_ALIGN_CENTER,
+};
+
 class CDrawingWrapper
 {
 public:
@@ -12,4 +21,6 @@ public:
 	void DrawLine(float x1, float y1, float x2, float y2, ImColor color, float thickness = 1.f);
 	void DrawLine(const ImVec2& start, const ImVec2& end, ImColor col, float thickness = 1.f);
 	void DrawWorldLine(const Vector& start, const Vector& end, ImColor col, float thickness = 1.f);
+	void DrawTextA(const char* text, const ImVec2& pos, ImColor col, TextAlign alignx = TEXT_ALIGN_LEFT, TextAlign aligny = TEXT_ALIGN_TOP);
+	void DrawTextScreenA(const char* text, const Vector& pos, ImColor col, TextAlign alignx = TEXT_ALIGN_LEFT, TextAlign aligny = TEXT_ALIGN_TOP);
 };
