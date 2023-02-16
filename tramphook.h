de3abc0732175472f65a253eb0fb21645f866dde
RegiSimkus <60613196+BullyHunter32@users.noreply.github.com>
2023-02-16 10:05:07 +0000

    Removed TrampHook testing dummy

--- tramphook.h
+++ tramphook.h
@@ -2,20 +2,5 @@
 #include <Windows.h>
 #include "debug.h"
 
-class Dummy
-{
-public:
-	int total = 0;
-
-
-};
-
-static void __stdcall DummyFunction()
-{
-	g_pDebug->Print("Im a dummy function!\n");
-}
-
-void __stdcall DummyHook();
-
 uintptr_t TrampHook(PVOID targetfunction, PVOID hook, SIZE_T bytes);
 void RestoreTrampHook(PVOID original, PVOID source, SIZE_T bytes);
